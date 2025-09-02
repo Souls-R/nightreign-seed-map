b2f08f7778e800a71ce0429f76bb14f182e83919
Souls-R <2362912722@qq.com>
2025-09-02 12:55:46 +0800

    mobile tab theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="theme-color" content="#131008" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
